1fb11b4b489065814f62450a655030b62dcf943e
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-03-02 14:54:58 -0600

    feat: build header component
    made header responsive
    
    header works on all pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,138 @@
+// react
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+
+// hamburger
+import { Squash as Hamburger } from "hamburger-react";
+
+// images
+import logo from "../assets/images/logo.png";
+
 const Header = () => {
-  return <div>Header</div>;
+  const [isOpen, setOpen] = useState(false);
+
+  return (
+    <>
+      <header className="relative bg-white text-black lg:rounded-2xl flex items-center justify-between p-5 z-50 max-w-[1100px] mx-auto">
+        <div>
+          <Link to={"/"}>
+            <img src={logo} alt="logo" className="w-28 md:w-44" />
+          </Link>
+        </div>
+
+        <ul className="list-none hidden lg:flex items-center gap-8">
+          <li>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/about"}
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/solutions"}
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              Our Solutions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/"}
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              Resources
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to={"/faqs"}
+              className={({ isActive }) =>
+                isActive ? "nav-link active" : "nav-link"
+              }
+            >
+              FAQ
+            </NavLink>
+          </li>
+        </ul>
+
+        <div className="flex items-center gap-5">
+          <div className="text-sm font-semibold">
+            <Link
+              to={"/"}
+              className="bg-primary text-white px-4 py-[10px] md:py-5 rounded-md border border-transparent transition-all duration-300 hover:bg-transparent hover:border-primary hover:text-primary"
+            >
+              Get in touch
+            </Link>
+          </div>
+
+          <div className="block lg:hidden">
+            <Hamburger toggled={isOpen} toggle={setOpen} size={28} />
+          </div>
+        </div>
+      </header>
+
+      <ul className={isOpen ? "nav-menu" : "nav-menu active"}>
+        <li>
+          <NavLink
+            to={"/"}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/about"}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/"}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            Our Solutions
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/faqs"}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+          >
+            FAQ
+          </NavLink>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0E387A",
+      },
+      container: {
+        center: true,
+      },
+    },
   },
   plugins: [],
 };
