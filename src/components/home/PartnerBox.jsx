ac43276038925b2063342b3af28c9094767f65ca
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-03-11 21:50:49 -0500

    feat: redevelop trusted section on home page

---
+++ src/components/home/PartnerBox.jsx
@@ -0,0 +1,19 @@
+// react
+import PropTypes from "prop-types"
+
+const PartnerBox = ({ img }) => {
+  return (
+    <div
+      className="p-4 sm:p-5  rounded-lg border-[0.52px] border-[
+      #f1f3f7] flex place-content-center partner-image"
+    >
+      <img src={img} alt="partner-logo" className="h-[20px] md:h-[30px]" />
+    </div>
+  )
+}
+
+PartnerBox.propTypes = {
+  img: PropTypes.string.isRequired
+}
+
+export default PartnerBox
