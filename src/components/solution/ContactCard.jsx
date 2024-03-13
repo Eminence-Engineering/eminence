ad2135099b182a9eda89629157aac85297b6a7a9
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-03-13 14:06:47 -0500

    feat: redevelop contact-card component on solutions page

--- src/components/solution/ContactCard.jsx
+++ src/components/solution/ContactCard.jsx
@@ -1,21 +1,31 @@
+// images
 import force from "../../assets/images/forcefields.png"
 
 const ContactCard = () => {
   return (
-    <section className="relative bg-white text-black lg:rounded-2xl shadow-inner flex items-center justify-between gap-6 px-24 p-5 md:py-7 z-50 max-w-6xl -mb-4 z-40 mx-auto">
-      <div className="p-4 ">
-        <h1 className="py-2 text-xl font-semibold text-black"> Contact Us </h1>
-        <p className="py-2 text-3xl font-semibold text-black">Get In Touch With Us</p>
-        <div className="flex-grow max-w-24 border-2 border-blue-800"></div>
-      </div>
-      <div>
-        <img src={force} alt="hero" />
+    <section className="relative text-black pt-24">
+      <div className="px-0 md:px-5">
+        <div className="container bg-white flex flex-col md:flex-row items-center justify-between gap-6 md:rounded-2xl z-40 relative md:-bottom-24 contact-card md:px-20 py-16">
+          <div className="text-center md:text-left">
+            <h1 className="text-xl font-medium text-black mb-4"> Contact Us </h1>
+            <p className="text-2xl md:text-3xl font-semibold text-black mb-4">Get In Touch With Us</p>
+            <div className="flex-grow max-w-24 border-2 border-primary mx-auto md:mx-0"></div>
+          </div>
+
+          <div className="hidden lg:block">
+            <img src={force} alt="hero" />
+          </div>
+
+          <div className="text-center md:text-left">
+            <p className="text-lg text-black mb-6">Lorem ipsum dolor sit amet consectetur.</p>
+            <button className="w-full max-w-48 text-lg font-bold rounded-lg px-8 text-white py-3 bg-primary mx-auto md:mx-0">
+              Contact Us
+            </button>
+          </div>
+        </div>
       </div>
 
-      <div className="p-4">
-        <p className="py-2 text-lg text-black">Lorem ipsum dolor sit amet consectetur. </p>
-        <button className="w-full max-w-48 text-base rounded-lg ... px-8 text-white py-3 bg-[#0E387A]">Contact Us</button>
-      </div>
+      <div className="w-full h-32 bg-primary hidden md:block relative -bottom-1"></div>
     </section>
   )
 }
